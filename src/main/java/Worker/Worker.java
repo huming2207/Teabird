4e915d842e29f0c05a64c605fa3c1a431d0a06da
Jackson Ming Hu <huming2207@gmail.com>
2018-03-02 15:05:17 +1100

    worker: initially implement filtered stream
    Signed-off-by: Jackson Ming Hu <huming2207@gmail.com>

--- src/main/java/Worker/Worker.java
+++ src/main/java/Worker/Worker.java
@@ -1,25 +1,96 @@
 package Worker;
 
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
 import twitter4j.auth.AccessToken;
+
 
 public class Worker
 {
-    private Twitter twitterClient;
+    private boolean isStreamStarted = false;
+    private AccessToken accessToken;
+    private String consumerToken;
+    private String consumerSecret;
+    private FilterQuery filterQuery;
 
-    public Worker()
+    public FilterQuery getFilterQuery()
     {
-        // Initialise twitter client
-        TwitterFactory twitterFactory = new TwitterFactory();
-        twitterClient = twitterFactory.getInstance();
+        return filterQuery;
+    }
+
+    public void setFilterQuery(FilterQuery filterQuery)
+    {
+        this.filterQuery = filterQuery;
     }
 
     public void login(AccessToken accessToken, String consumerToken, String consumerSecret)
     {
-        twitterClient.setOAuthConsumer(consumerToken, consumerSecret);
-        twitterClient.setOAuthAccessToken(accessToken);
+        this.accessToken = accessToken;
+        this.consumerToken = consumerToken;
+        this.consumerSecret = consumerSecret;
     }
 
+    public void createStream()
+    {
+        isStreamStarted = true;
 
+        StatusListener statusListener = new StatusListener()
+        {
+            @Override
+            public void onStatus(Status status)
+            {
+
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice)
+            {
+
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses)
+            {
+
+            }
+
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId)
+            {
+
+            }
+
+            @Override
+            public void onStallWarning(StallWarning warning)
+            {
+
+            }
+
+            @Override
+            public void onException(Exception ex)
+            {
+                ex.printStackTrace();
+                isStreamStarted = false;
+            }
+        };
+
+
+        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        twitterStream.setOAuthAccessToken(this.accessToken);
+        twitterStream.setOAuthConsumer(this.consumerToken, this.consumerSecret);
+
+        // Start to run the stream crawling!
+        twitterStream.addListener(statusListener);
+
+        if(filterQuery != null) {
+            twitterStream.filter(filterQuery);
+        } else {
+            twitterStream.sample();
+        }
+
+    }
+
+    public boolean isStreamStarted()
+    {
+        return isStreamStarted;
+    }
 }
---
+++ src/main/java/Worker/WorkerConfig/FilterLevel.java
@@ -0,0 +1,19 @@
+package Worker.WorkerConfig;
+
+public enum FilterLevel
+{
+    LOW {
+        public String toString()
+        {
+            return "low";
+        }
+    },
+
+    MEDIUM {
+        public String toString()
+        {
+            return "medium";
+        }
+    }
+
+}
