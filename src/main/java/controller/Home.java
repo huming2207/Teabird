5f11313a34e02c6480c757b120e184afa897bbd6
Jackson Ming Hu <huming2207@gmail.com>
2018-03-04 10:55:06 +1100

    Gui: implement controller
    Signed-off-by: Jackson Ming Hu <huming2207@gmail.com>

--- src/main/java/controller/Home.java
+++ src/main/java/controller/Home.java
@@ -1,6 +1,47 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import worker.TeabirdApplication;
+
 public class Home
 {
+    @FXML
+    private TextField consumerTokenTextfield;
 
+    @FXML
+    private TextField consumerSecretTextfield;
+
+    @FXML
+    private TextField accessTokenTextfield;
+
+    @FXML
+    private TextField accessSecretTextfield;
+
+    @FXML
+    private TextField keywordListPathTextfield;
+
+    @FXML
+    private TextField useridListPathTextfield;
+
+    @FXML
+    private TextField outputPathTextfield;
+
+    @FXML
+    private TextArea outputTextarea;
+
+    private TeabirdApplication teabirdApplication;
+
+    public void setMainApp(TeabirdApplication teabirdApplication)
+    {
+        this.teabirdApplication = teabirdApplication;
+    }
+
+    public void appendDebugText(String text)
+    {
+        Platform.runLater(() -> outputTextarea.appendText(text));
+    }
 }
